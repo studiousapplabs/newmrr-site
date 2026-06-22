8fb0d908df6ecd95bd955bd9fcafbb1871b6dd6a
studiousapplabs <292149581+studiousapplabs@users.noreply.github.com>
2026-06-22 13:53:45 -0400

    Add site config for Stripe link

---
+++ assets/config.js
@@ -0,0 +1,7 @@
+window.NEWMRR_CONFIG = {
+  // Paste your Stripe Payment Link here.
+  // In Stripe, set the after-payment redirect to:
+  // https://studiousapplabs.github.io/newmrr-site/success.html
+  stripePaymentLink: 'REPLACE_WITH_STRIPE_PAYMENT_LINK',
+  leadEmail: 'studiousapplabs@gmail.com'
+};
